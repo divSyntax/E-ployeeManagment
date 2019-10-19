c454ceaf464f65b1549e747b27301207959e491b
DivSyntax <divsyntax@gmail.com>
2019-10-18 21:04:39 -0500

    Added SQL query class.

---
+++ Models/SQLCommands.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EmployeeManagment.Models
+{
+    public class SQLCommands : IEmployeeRepo
+    {
+        private readonly EmpDbContext empDbContext;
+
+        public SQLCommands(EmpDbContext empDbContext)
+        {
+            this.empDbContext = empDbContext;
+        }
+
+        public Employee Add(Employee employee)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Employee DeleteEmp(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<Employee> getAllEmployees()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Employee getEmployee(int Id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Employee Update(Employee updateEmp)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
