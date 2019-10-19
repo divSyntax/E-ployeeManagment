77f397c7ee5b0c81c598d48b287e0e305c4b5c88
DivSyntax <divsyntax@gmail.com>
2019-10-18 19:51:58 -0500

    Added data access layer. Added DataSet Employee model. Added Entity .NET Core.

---
+++ Models/EmpDbContext.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EmployeeManagment.Models
+{
+    public class EmpDbContext : DbContext
+    {
+
+        private DbSet<Employee> employees;
+
+        public DbSet<Employee> Employees
+        {
+            get { return employees; }
+            set { employees = value; }
+        }
+
+        public EmpDbContext(DbContextOptions<EmpDbContext> options) : base(options)
+        {
+           
+        }
+    }
+}
